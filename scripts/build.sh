b46e9b31d63e1372ab4b69212c224c2b1e847edc
lino <luizgustavolino@gmail.com>
2020-09-07 14:36:37 -0300

    adding more logs

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,10 @@ USERNAME=$(echo $JSON | grep -o '"user" *: *"[^"]*' | cut -d'"' -f4)
 PASSWORD=$(echo $JSON | grep -o '"password" *: *"[^"]*' | cut -d'"' -f4)
 DEVICE=$(echo $JSON | grep -o '"device" *: *"[^"]*' | cut -d'"' -f4)
 
+lv-cli log -i "cloud login for $DEVICE"
 particle login -u $USERNAME -p $PASSWORD
+
+lv-cli log -i "now compiling..."
 particle flash $DEVICE /lv/game.ino /lv/
+
+lv-cli log -i "ready"
